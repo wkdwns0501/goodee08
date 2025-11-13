7d3862c0a218f01776e32b63e5ae57788133f414
wkdwns0501 <britain0501@naver.com>
2025-11-13 10:54:03 +0900

    논리 연산자

---
+++ chap03/src/sec02/exam09/LogicalOperatorEx.java
@@ -0,0 +1,26 @@
+package sec02.exam09;
+
+public class LogicalOperatorEx {
+	public static void main(String[] args) {
+		int charCode = 'A';
+		System.out.println(charCode); // 65
+		
+		if ((charCode >= 65) & (charCode <= 90)) { // true & true -> true
+			System.out.println("대문자");
+		}
+		if ((charCode >= 97) && (charCode <= 122)) { // false && -> false (short-circuit 동작)
+			System.out.println("소문자");
+		}
+		if ((charCode >= 48) && (charCode <= 57)) { // true && false -> false
+			System.out.println("소문자");
+		}
+		int value = 6;
+		if ((value % 2 == 0) | (value % 3 == 0)) { //  true | true -> true (뒤에 로직까지 검사)
+			System.out.println("2또는 3의 배수");
+		}
+		if ((value % 2 == 0) || (value % 3 == 0)) { //  true && -> true (short-circuit 동작)
+			System.out.println("2또는 3의 배수");
+		}
+		// 정리: 연산의 결과는 같은데 효율성(속도)이 더 높으므로 &&, || 사용
+	}
+}
