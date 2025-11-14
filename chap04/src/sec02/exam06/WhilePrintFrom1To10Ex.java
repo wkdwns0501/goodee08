cd42d46cb4f35115c82a7e58be2bb353de95e0a6
wkdwns0501 <britain0501@naver.com>
2025-11-14 12:30:49 +0900

    while 문

---
+++ chap04/src/sec02/exam06/WhilePrintFrom1To10Ex.java
@@ -0,0 +1,12 @@
+package sec02.exam06;
+
+public class WhilePrintFrom1To10Ex {
+	public static void main(String[] args) {
+		// 1부터 10까지 반복해서 출력
+		int i = 1; // 초기화식
+		while (i <= 10) { // 조건식
+			System.out.println(i);
+			i++; // 증감식
+		}
+	}
+}
