a2e335f4d88422c46ad11c826c0d3ea33f225679
wkdwns0501 <britain0501@naver.com>
2025-11-18 16:05:30 +0900

    메소드

---
+++ chap06/src/sec04/exam01/CalculatorEx.java
@@ -0,0 +1,19 @@
+package sec04.exam01;
+
+public class CalculatorEx {
+	public static void main(String[] args) {
+		Calculator myCalc = new Calculator(); // 기본 생성자 호출
+		myCalc.powerOn();
+		
+		int result1 = myCalc.plus(5,6);
+		System.out.println("result1: " + result1);
+		
+		byte x = 10;
+		byte y = 4;
+		double result2 = myCalc.divide(x, y); // int 로 자동 타입 변환
+		System.out.println("result2: " + result2);
+		
+//		double result = myCal?c.divide(10.5, 20.0); //잘못된 매개값 사용;
+		myCalc.powerOff();
+	}
+}
