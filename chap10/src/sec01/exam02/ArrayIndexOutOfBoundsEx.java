59eb387a9c2d036d138b558b07421e4b0b07e061
wkdwns0501 <britain0501@naver.com>
2025-11-25 17:05:51 +0900

    ArrayIndexOutOfBounds 예외

---
+++ chap10/src/sec01/exam02/ArrayIndexOutOfBoundsEx.java
@@ -0,0 +1,17 @@
+package sec01.exam02;
+
+public class ArrayIndexOutOfBoundsEx {
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 3};
+		int result = arr[0] + arr[2];
+//		int result = arr[0] + arr[3]; // 예외 발생
+		
+		System.out.println(result);
+		
+		String data1 = args[0];
+		String data2 = args[1];
+		
+		System.out.println("args[0]: " + data1);
+		System.out.println("args[1]: " + data2);
+	}
+}
