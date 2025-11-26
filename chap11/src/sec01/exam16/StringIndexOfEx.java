cb5cb66569b877e03a3463735db38312b1cf8ac9
wkdwns0501 <britain0501@naver.com>
2025-11-26 17:30:22 +0900

    indexOf() 메소드

---
+++ chap11/src/sec01/exam16/StringIndexOfEx.java
@@ -0,0 +1,28 @@
+package sec01.exam16;
+
+public class StringIndexOfEx {
+	public static void main(String[] args) {
+		String subject = "자바 프로그래밍";
+		String subject2 = "자바 프로그래밍 자바 웹개발";
+		
+		int location = subject.indexOf("프로그래밍");
+		System.out.println(location);
+		System.out.println(subject.indexOf("자바"));
+		System.out.println(subject2.indexOf("자바", 2));
+		System.out.println(subject2.indexOf("자바", 20));
+		
+		if (subject.indexOf("자바") != -1) {
+			System.out.println("자바와 관련된 책");
+		}else {
+			System.out.println("자바와 관련 없는 책");
+		}
+		
+		// (참고) 문자열 포함 여부를 알려주는 메소드
+		if (subject.contains("자바")) {
+			System.out.println("자바와 관련된 책");
+		} else {
+			System.out.println("자바와 관련 없는 책");
+		}
+		
+	}
+}
