81498f2345296d56a20099d8c8c057ab94418498
wkdwns0501 <britain0501@naver.com>
2025-11-26 17:06:39 +0900

    API

---
+++ chap11/src/sec01/exam01/APIDocument.java
@@ -0,0 +1,9 @@
+package sec01.exam01;
+
+public class APIDocument {
+	public static void main(String[] args) {
+		String str = "문자열";
+		// String 클릭하고 F1누르고 help에 Javadoc for 'java.lang.String' 
+		// 누르면 help 탭에 API사이트가 열림
+	}
+}
