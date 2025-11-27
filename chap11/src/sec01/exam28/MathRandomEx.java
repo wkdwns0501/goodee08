5274106681d9e5ab712629d2f0c8207ef649065d
wkdwns0501 <britain0501@naver.com>
2025-11-27 17:35:29 +0900

    Math.random을 활용한 랜덤값 (공식화)

---
+++ chap11/src/sec01/exam28/MathRandomEx.java
@@ -0,0 +1,19 @@
+package sec01.exam28;
+
+public class MathRandomEx {
+	public static void main(String[] args) {
+		// 공식화: (int) (Math.random() * (최대값 - 최소값 + 1)) + 최소값;
+		
+		// 1부터 10까지 임의의 정수 난수 얻기
+		int num = (int) (Math.random() * 10) + 1;
+		System.out.println("num: " + num);
+		
+		// 주사위 번호 뽑기(1, 2, 3, 4, 5, 6)
+		int dice = (int) (Math.random() * 6) + 1;
+		System.out.println("주사위 눈: " + dice);
+		
+		// 로또 번호 뽑기(1, 2, 3, ... 43, 44, 45)
+		int lotto = (int) (Math.random() * 45) + 1;
+		System.out.println("로또 번호: " + lotto);		
+	}
+}
