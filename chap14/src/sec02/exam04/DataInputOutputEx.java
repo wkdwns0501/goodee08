e74f69e0701dec426e35fa0a752d9f3cc55cc4bd
wkdwns0501 <britain0501@naver.com>
2025-12-07 16:07:02 +0900

    데이터 입출력

---
+++ chap14/src/sec02/exam04/DataInputOutputEx.java
@@ -0,0 +1,41 @@
+package sec02.exam04;
+
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class DataInputOutputEx {
+	public static void main(String[] args) throws Exception {
+		FileOutputStream fos = new FileOutputStream("C:/Temp/primitive.txt");
+		DataOutputStream dos = new DataOutputStream(fos);
+		
+		// 기본 타입 값 출력
+		dos.writeUTF("홍길동");
+		dos.writeDouble(95.5);
+		dos.writeInt(1);
+		
+		dos.writeUTF("김재현");
+		dos.writeDouble(90.3);
+		dos.writeInt(2);
+		
+		dos.flush();
+		dos.close();
+		
+		FileInputStream fis = new FileInputStream("C:/Temp/primitive.txt");
+		DataInputStream dis = new DataInputStream(fis);
+		
+		for (int i = 0; i < 2; i++) {
+			// 기본 타입 값 읽기
+			// (주의) 출력한 순서와 동일한 순서로 읽어야 함
+			String name = dis.readUTF();
+			double score = dis.readDouble();
+			int order = dis.readInt();
+			System.out.println(name + " : " + score + " : " + order);
+		}
+		
+		dis.close(); 
+		
+	}
+}
